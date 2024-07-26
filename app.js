a24f28611b846d3198e167df422aa7f0e8d2fc3b
Melbylau2469 <arthur.alberti@escola.pr.gov.br>
2024-07-26 12:08:42 -0300

    c

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+alert('Boas vindas ao jogo mais gay de todos os tempos');
+let numeroSecreto = 69;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 100');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+alert('Isso ai! Você virou um gay');
 } else {
-alert('Você errou :(')
+alert('Você fez o l :(')
 }
